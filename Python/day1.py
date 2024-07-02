7448dc434c763d3adc639b5e7811bc1d66e76390
CenT3003 <165035208+CenT3003@users.noreply.github.com>
2024-07-02 22:35:34 +0400

    Add files via upload
    пидорасы

---
+++ Python/day1.py
@@ -0,0 +1,68 @@
+'''
+first = input('Введите 1 число: ')
+second = input('Введите 2 число: ')
+third = input('Введите 3 число: ')
+print('Площадь равна = ', first+second+third) 
+'''
+
+'''
+a = int(input())
+if a%2 == 1:
+    print('Odd number')
+else:
+    print('Even number')
+'''
+
+'''
+a = int(input())
+if a%7 == 0:
+    print('Number is multiple 7.')
+else:
+    print('ENumber is not multiple 7') 
+'''
+
+'''
+first = int(input('Введите 1 число: '))
+second = int(input('Введите 2 число: '))
+shoose = int(input('Введи числа: 1 - сумма, 2 - разница, 3 - среднеарифметическое, 4 - произведение: '))
+if shoose == 1:
+    print(first+second)
+elif shoose == 2:
+    print(first-second)
+elif shoose == 3:
+    print((first+second)/2)
+elif shoose == 4:
+    print(first*second)
+'''
+
+'''
+first = int(input('Введите 1 число: '))
+second = int(input('Введите 2 число: '))
+
+while first<=second:
+    if first%2 == 0:
+        print(first)
+    first+=1
+'''
+
+first = int(input('Введите 1 число: '))
+second = int(input('Введите 2 число: '))
+one = 0
+'''
+while first<=second:
+    print(first)
+    first+=1
+'''
+'''
+while second>=first:
+    print(second)
+    second-=1
+    '''
+
+'''
+while first <= second:
+    if first%5 == 0:
+        one+=1
+    first+=1
+print(one)
+'''
